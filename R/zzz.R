763a4098d01ea33a9789005afe6435b9af6f7fab
unknown <unknown@email>
2004-09-02 18:10:19 +0000

    This commit was manufactured by cvs2svn to create branch 'RELEASE_1_4_branch'.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_1_4_branch/madman/Rpacks/graph@9859 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+
+.First.lib <- function(libname, pkgname, where) {
+
+    ##load time code
+    where = match(paste("package:", pkgname, sep=""), search())
+    .initGContents(where)
+
+ if((.Platform$OS.type == "windows") && ("Biobase" %in% installed.packages()[,"Package"])
+    && (interactive()) && (.Platform$GUI ==  "Rgui")){
+     if (require("Biobase"))
+         addVigs2WinMenu("graph")
+ }
+
+}
